1162242389aa3c1be32fa2873a62a2318ff248c5
aarondelgiudice <aarondelgiudice@gmail.com>
2022-01-23 21:14:31 -0500

    add solution for integer-to-roman; source: https://leetcode.com/problems/integer-to-roman/

---
+++ integerToRoman.py
@@ -0,0 +1,101 @@
+"""
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral.
+
+ 
+
+Example 1:
+
+Input: num = 3
+Output: "III"
+Explanation: 3 is represented as 3 ones.
+Example 2:
+
+Input: num = 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+Example 3:
+
+Input: num = 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+Constraints:
+
+1 <= num <= 3999
+
+source: https://leetcode.com/problems/integer-to-roman/
+"""
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman_numerals = {
+            1       : "I", 
+            4       : "IV",
+            5       : "V",
+            9       : "IX",
+            10      : "X",
+            40      : "XL",
+            50      : "L",
+            90      : "XC",
+            100     : "C",
+            400     : "CD",
+            500     : "D",
+            900     : "CM",
+            1000    : "M"
+        }
+        
+        seq = list(roman_numerals.keys())[::-1]  # e.g. [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        stack = []  # List: loop through numeric values in num and convert to roman numeral
+        idx = 0  # use to track magnitude of num (also position in seq)
+               
+        # loop through num until it's zero'd out
+        while num > 0:
+            # find magnitude of num; e.g. 10s 100s 1000s
+            if num >= seq[idx]:
+                # convert numeric to roman numeral
+                stack.append(roman_numerals[seq[idx]])
+                num -= seq[idx]  # this will eventually set num == 0
+
+            else:
+                idx += 1  # skip ahead until num magnitude matches sequence
+
+        return "".join(stack)  # convert list to string
+
+
+# -----------------------------------------------------------------------------
+# run solution
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+    INPUTS = (
+        # input: int,   output: str
+        (3,             "III"),
+        (58,            "LVIII"),
+        (1994,          "MCMXCIV")
+    )
+
+    mySolution = Solution()
+
+    for num, expected in INPUTS:
+        print(f"input: {num}")
+        actual = mySolution.intToRoman(num)
+        print(f"expected: {expected}, actual: {actual}")
+        if expected != actual:
+            raise RuntimeError(f"acutal value does not match expected: actual={actual}, expected={expected}")
