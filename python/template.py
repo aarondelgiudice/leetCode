6acb8c82eac2c330cbbfcdfac0857fa823a39ed7
Aaron Del Giudice <aarondelgiudice@gmail.com>
2024-05-28 15:35:53 -0400

    template for new python problems

---
+++ python/template.py
@@ -0,0 +1,22 @@
+"""<number>. <Problem Name> | <Difficulty>"""
+
+from utils.test_solution import test
+
+
+def main():
+    # test cases
+    test_cases = [
+        # Tuple(inputs), expected
+    ]
+
+    test(test_cases, solution)
+
+    return
+
+
+def solution():
+    pass
+
+
+if __name__ == "__main__":
+    main()
