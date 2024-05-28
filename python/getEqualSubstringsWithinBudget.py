c7f07764efaebfb2c5183d4b9c31fbaa68263e99
Aaron Del Giudice <aarondelgiudice@gmail.com>
2024-05-28 15:30:59 -0400

    daily: get equal substrings within a budget

--- python/getEqualSubstringsWithinBudget.py
+++ python/getEqualSubstringsWithinBudget.py
@@ -1,44 +1,6 @@
 """1208. Get Equal Substrings Within Budget | Medium"""
 
-import time
-
-from typing import List, Callable
-
-
-def test(test_cases: List, fn: Callable):
-    success = 0
-    start_time = time.time()
-
-    for inputs, expected in test_cases:
-        current_start_time = time.time()
-
-        actual = fn(*inputs)
-
-        current_end_time = time.time()
-        current_total_time = round(current_end_time - current_start_time, 4)
-
-        assert actual == expected, f"Expected {expected}, but got {actual}."
-
-        if actual == expected:
-            print(
-                f"✅ solution({inputs}) returned {actual}. Took {current_total_time}s."
-            )
-
-            success += 1
-
-        else:
-            print(
-                f"❌ solution({inputs}) returned {actual}, but expected {expected}."
-            )
-
-    end_time = time.time()
-    total_time = round(end_time - start_time, 4)
-
-    assert success == len(test_cases), f"❌ {len(test_cases)-success} test(s) failed."
-
-    print(f"\n✅ {success} test(s) passed! Took {total_time}s.")
-
-    return
+from utils.test_solution import test
 
 
 def main():
@@ -89,6 +51,7 @@ def solution(s: str, t: str, maxCost: int) -> int:
         # increment `right` pointer automatically
 
     return maxLen
+
 
 if __name__ == "__main__":
     main()
